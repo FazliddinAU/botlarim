86be813d985df2c782fd25f25d494be954e5f891
FAZLIDDIN <fazliddinauu@gmail.com>
2025-01-01 12:20:47 +0500

    Update request.js

--- request.js
+++ request.js
@@ -4,11 +4,11 @@ async function downloaderMethod(insta_url){
    try {
     const options = {
         method : 'GET',
-        url : 'https://instagram-scraper-api2.p.rapidapi.com/v1/highlight_info?highlight_id=17907964880010937' ,
+        url : 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php' ,
        params : {url : insta_url},
         headers : {
-           'X-RapidApi-Key': 'b12cfb0fb8msh5c3b6a9f4d10c4bp1bcae9jsn637f7d88bf4f',
-           'x-rapidapi-host': 'instagram-scraper-api2.p.rapidapi.com'  
+           'X-RapidApi-Key': '3fb0f031femshba140de95f307fdp1ddb9fjsn43c4694c0a30',
+           'x-rapidapi-host': 'instagram-media-downloader.p.rapidapi.com'  
         }
        };
 
