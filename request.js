9782aa4dd30d1f56f91616fe83c5a2fd0d61260b
FAZLIDDIN <fazliddinauu@gmail.com>
2025-02-04 12:11:36 +0500

    Update request.js

--- request.js
+++ request.js
@@ -1,35 +1,29 @@
-const axios = require("axios");
+const axios = require('axios');
 
-async function downloaderMethod(insta_url) {
-  try {
-    const options = {
-      method: 'GET',
-      url: 'https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/get-info-rapidapi',
-      params: { url: insta_url },
-      headers: {
-        'X-RapidAPI-Key': '599dc14713msh1e8b59825e43e3ap19f9cfjsn1fc776d4c34e',
-        'X-RapidAPI-Host': 'instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com'
-      }
-    };
-
-    const response = await axios.request(options);
-    console.log('API Response:', response.data);
-
-    
-    const result = {
-      videoUrl: response.data.video || null,   // Video URL
-      imageUrl: response.data.image || null,  // Rasm URL
-      storyUrl: response.data.story || null,  // Hikoya URL
-      caption: response.data.caption || null, // Rasm yoki video izohi
-    };
-
-    return result;
-  } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
-    return null;
+async function downloaderMethod(insta_url){
+ try{
+  const options = {
+    method: 'GET',
+    url: 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php',
+    params: {url: insta_url},
+    headers: {
+      'x-rapidapi-key': '334e48845dmsh76943f62ec84287p155053jsn2af2e2f50c2c',
+      'x-rapidapi-host': 'instagram-media-downloader.p.rapidapi.com'
+    }
+  };
+  const response = await axios.request(options)
+  const result = {
+    videoUrl : response.data.video,
+    caption : response.data.caption
   }
-}
+  return result
 
+
+
+ }catch(error){
+ console.log(error+'');
+ }
+}
 module.exports = {
   downloaderMethod
-};
+}
