f89fb292fb7d59f13e43e497035ce9fd5c2f45d9
FAZLIDDIN <fazliddinauu@gmail.com>
2025-02-05 16:31:44 +0500

    Update request.js

--- request.js
+++ request.js
@@ -1,29 +1,33 @@
 const axios = require('axios');
 
-async function downloaderMethod(insta_url){
- try{
+async function downLoaderMethod(insta_url){
+try {
   const options = {
     method: 'GET',
-    url: 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php',
-    params: {url: insta_url},
+    url: 'https://instagram-story-downloader-media-downloader.p.rapidapi.com/index',
+    params: {url: insta_url },
     headers: {
       'x-rapidapi-key': '334e48845dmsh76943f62ec84287p155053jsn2af2e2f50c2c',
-      'x-rapidapi-host': 'instagram-media-downloader.p.rapidapi.com'
+      'x-rapidapi-host': 'instagram-story-downloader-media-downloader.p.rapidapi.com'
     }
   };
+
   const response = await axios.request(options)
   const result = {
-    videoUrl : response.data.video,
+    videoUrl : response.data.media,
     caption : response.data.caption
   }
+
   return result
 
 
-
- }catch(error){
- console.log(error+'');
- }
+} catch (error) {
+  console.log(error+"");
 }
+  
+
+}
+
 module.exports = {
-  downloaderMethod
+  downLoaderMethod
 }
